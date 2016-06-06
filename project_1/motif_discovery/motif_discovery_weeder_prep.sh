f9cee10a6e6bed098556cf0805cabc7a613e6bbd
Naoto-Imamachi <akimitsu@sc090.(none)>
2016-06-06 23:44:26 +0900

    test

--- project_1/motif_discovery/motif_discovery_weeder_prep.sh
+++ project_1/motif_discovery/motif_discovery_weeder_prep.sh
@@ -8,6 +8,14 @@
 #Input files - required
 AllFastaFile="/home/akimitsu/database/List_1_mRNA_signifFC2.txt_infection_3UTR.fa"
 
-wfrequency_maker List_1_mRNA_signifFC2.txt_infection_3UTR.fa KI
+wfrequency_maker List_1_mRNA_signifFC2.txt_all_3UTR.fa KI
 mv KI.6.freq /home/akimitsu/software/Weeder1.4.2/FreqFiles
 mv KI.8.freq /home/akimitsu/software/Weeder1.4.2/FreqFiles
+
+wfrequency_maker List_2_lncRNA_signifFC2.txt_all_lncRNA.fa LN
+mv LN.6.freq /home/akimitsu/software/Weeder1.4.2/FreqFiles
+mv LN.8.freq /home/akimitsu/software/Weeder1.4.2/FreqFiles
+
+wfrequency_maker List_mix_PROMPT_eRNA_SLiT_lncRNA.txt_all_lncRNA_eRNA_PROMPT.fa SL
+mv SL.6.freq /home/akimitsu/software/Weeder1.4.2/FreqFiles
+mv SL.8.freq /home/akimitsu/software/Weeder1.4.2/FreqFiles
--- project_1/motif_discovery/motif_discovery_weeder_test.sh
+++ project_1/motif_discovery/motif_discovery_weeder_test.sh
@@ -6,6 +6,10 @@
 #$ -l mem_req=8G
 
 #Input files - required
-InputFile="/home/akimitsu/database/List_1_mRNA_signifFC2.txt_infection_3UTR.fa"
+InputFile1="/home/akimitsu/database/List_1_mRNA_signifFC2.txt_infection_3UTR.fa"
+InputFile2="/home/akimitsu/database/List_2_lncRNA_signifFC2.txt_infection_lncRNA.fa"
+InputFile3="/home/akimitsu/database/List_mix_PROMPT_eRNA_SLiT_lncRNA.txt_infection_SLiT_eRNA_PROMPT.fa"
 
-./weederlauncher.out ${InputFile} KI small
+./weederlauncher.out ${InputFile1} KI small
+./weederlauncher.out ${InputFile2} LN small
+./weederlauncher.out ${InputFile3} SL small
