6a7b65640e6c02cd2548b955004d3e961b0c891c
Naoto-Imamachi <akimitsu@sc091.(none)>
2016-06-12 14:25:06 +0900

    Add strand-specific RNA-seq workflow

--- project_1/edgeR_test_for_MTR4/featureCounts_strand_specific-edgeR_test.sh
+++ project_1/edgeR_test_for_MTR4/featureCounts_strand_specific-edgeR_test.sh
@@ -5,5 +5,5 @@
 #$ -l s_vmem=16G
 #$ -l mem_req=16G
 
-#python3 featureCounts_strand_specific-edgeR_test.py sample_table_siMTR4_7.txt
+python3 featureCounts_strand_specific-edgeR_test.py sample_table_siMTR4_7.txt
 python3 featureCounts_strand_specific-edgeR_test.py sample_table_siMTR4_8.txt
